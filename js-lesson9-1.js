fe5ff5b791a384ec78cdc0895446fc37427ef8a8
rainik <itrainik@gmail.com>
2015-11-19 01:24:42 +0200

    Часть домашки
    Обновление

---
+++ js-lesson9-1.js
@@ -0,0 +1,11 @@
+//Task #2
+var a = +prompt("input first number","");
+var b = +prompt("input second number","");
+var sum = a
+if (a > b) {
+	for (var i = a; i = b; i++){
+		var sum = sum + i;
+		
+	}
+	alert("Amount " + sum);
+}
--- js-lesson9.js
+++ js-lesson9.js
@@ -1,2 +1,66 @@
 ﻿//Task #1
+/*
+var a = +prompt("input first number","");
+var b = +prompt("input second number","");
+var arr = [];
+var n = 0;
+for (a ; a <= b; a++) {
+		 arr[n] = a;
+		 n+=1;		
+		}
+alert("The numerical series: " + arr + "    Amount of numbers in the row: " + arr.length);
+*/
+
+//Task #2
+/*
+var a = +prompt("input first number","");
+var b = +prompt("input second number","");
+var sum = 0;
+for (a ; a <= b ; a++) {
+	sum += a;		
+	}
+alert("The elements sum is: " + sum);
+*/
+
+// Task #3
+/*
+var n = +prompt("input a number","");
+var fact = 1;
+for (var i = 1 ; i <= n ; i++) {
+	fact *= i;
+}
+alert(fact);
+*/
+
+//Task #4
+/*
+var a = +prompt("input first number","");
+var b = +prompt("input second number","");
+while (a >= b) {
+	a = a - b;
+}
+alert("The length of remaining segment is: " + a);
+*/
+
+// Task #5
+/*
+var n = +prompt("input a number","");
+var k = 1;
+while (n/3 >= k) {
+	k++;
+}
+alert("At least 'k' should be equal: " + k);
+*/
+
+//Task #6
+var n = +prompt("input a number","");
+
+
+
+
+
+
+
+
+
 
